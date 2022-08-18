12abbd72db7fdaf94838d6808eca33b0bee571c1
Anthony <anthonyledesma12@yahoo.com>
2022-08-17 19:53:40 -0700

    yes

---
+++ src/components/Layout/Header.js
@@ -0,0 +1,17 @@
+import { Fragment } from "react";
+
+const Header = props => {
+    return <Fragment>
+        <header
+        ><h1>Meals</h1>
+        <button>cart</button>
+        </header>
+       <div>
+        <img  src="" />
+       </div>
+        <div></div>
+    </Fragment>
+
+};
+
+export default Header;
