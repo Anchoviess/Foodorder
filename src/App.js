86e49c01d889266b0847b8d6157c86960f981605
Anthony <anthonyledesma12@yahoo.com>
2022-08-18 23:09:28 -0700

    working on the cart reducers for this puppy, should  be able to add items to the actual cart soon booyah

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import { Fragment, useState } from "react";
+import { useState } from "react";
 
 import Header from "./components/Layout/Header";
 import Meals from "./components/Meals/Meals";
 import Cart from "./components/Cart/Cart";
-
+import CartProvider from "./store/CartProvider";
 
 function App() {
   const [cartIsShown, setCartIsShown] = useState(false);
@@ -16,7 +16,7 @@ const hideCartHandler = () => {
 }
 
   return (
-    <Fragment>
+    <CartProvider>
       {cartIsShown && <Cart onclose={hideCartHandler} />}
       <Cart/>
       <Header onShownCart={showCartHandler} />
@@ -24,7 +24,8 @@ const hideCartHandler = () => {
       <main>
         <Meals/>
       </main>
-      </Fragment>
+      
+      </CartProvider>
   );
 };
 
