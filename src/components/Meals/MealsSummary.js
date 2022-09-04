bc5658c0a2995d16352e27631e30fd2766d95dfb
Anthony <anthonyledesma12@yahoo.com>
2022-09-03 23:13:20 -0700

    returned the values im trying to fetch

--- src/components/Meals/MealsSummary.js
+++ src/components/Meals/MealsSummary.js
@@ -1,20 +1,19 @@
 import classes from './MealsSummary.module.css';
 
 const MealsSummary = () => {
-
-    return (
-<section className={classes.summary}>
-      <h2>Delicious Food That i like to eat personally...</h2>
+  return (
+    <section className={classes.summary}>
+      <h2>Delicious Food, Delivered To You</h2>
       <p>
-        Choose my favorite meal from the broad selection of available meals
-        and enjoy a delicious lunch or dinner from the comfort of your imagination.
+        Choose your favorite meal from our broad selection of available meals
+        and enjoy a delicious lunch or dinner at home.
       </p>
       <p>
-        All my meals are cooked with high-quality ingredients, just-in-time and
-        of course by experienced chefs. I am that chef.
+        All our meals are cooked with high-quality ingredients, just-in-time and
+        of course by experienced chefs!
       </p>
     </section>
-    );
+  );
 };
 
 export default MealsSummary;
